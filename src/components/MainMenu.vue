a747fdd9db6930720ea64e1d9c0e4a9a85a31d08
Roman1510 <romanvinnick@gmail.com>
2021-11-21 21:30:38 +0100

    added graphics to the project

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,25 +1,40 @@
 <script>
+
+import easy from "../assets/images/buttons/difficulty/easy/easy.png"
+import easyPressed from "../assets/images/buttons/difficulty/easy/easy-pressed.png"
+import notSoEasy from "../assets/images/buttons/difficulty/not-so-easy/not-so-easy.png"
+import notSoEasyPressed from "../assets/images/buttons/difficulty/not-so-easy/not-so-easy-pressed.png"
+import easiest from "../assets/images/buttons/difficulty/easiest/easiest.png"
+import easiestPressed from "../assets/images/buttons/difficulty/easiest/easiest-pressed.png"
+import hard from "../assets/images/buttons/difficulty/hard/hard.png"
+import hardPressed from "../assets/images/buttons/difficulty/hard/hard-pressed.png"
 export default {
    name: "Main menu",
    setup: function() {
-       const handleChoice = (input)=>{
-           console.log(input)
+       const handleChoice = ()=>{
+
        }
 
        return {
-           handleChoice
+           handleChoice,
+           easy,
+           easyPressed,
+           notSoEasy,
+           notSoEasyPressed,
+           easiest,
+           easiestPressed,
+           hard,hardPressed
        }
    }
 }
 </script>
 <template>
     <div>
-        <button @click="handleChoice(1)">1</button>
-        <button @click="handleChoice(2)">2</button>
-        <button @click="handleChoice(3)">3</button>
-        <button @click="handleChoice(4)">4</button>
+        <img :src="easiestPressed" alt="">
     </div>
 </template>
 <style scoped>
-    
+    img{
+        width: 13rem;
+    }
 </style>
