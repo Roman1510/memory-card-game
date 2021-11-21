8abbe7a94c122238bdf8766fb4f5932b3e241143
Roman1510 <romanvinnick@gmail.com>
2021-11-21 22:45:17 +0100

    refactoerd MainMenu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,87 +1,101 @@
 <script>
+import { ref } from "@vue/reactivity";
 import mainMenu from "../assets/images/menu/main-menu.png";
 import easy from "../assets/images/buttons/difficulty/easy/easy.png";
-import easyPressed from "../assets/images/buttons/difficulty/easy/easy-pressed.png";
+//import easyPressed from "../assets/images/buttons/difficulty/easy/easy-pressed.png";
 import notSoEasy from "../assets/images/buttons/difficulty/not-so-easy/not-so-easy.png";
-import notSoEasyPressed from "../assets/images/buttons/difficulty/not-so-easy/not-so-easy-pressed.png";
+//import notSoEasyPressed from "../assets/images/buttons/difficulty/not-so-easy/not-so-easy-pressed.png";
 import easiest from "../assets/images/buttons/difficulty/easiest/easiest.png";
-import easiestPressed from "../assets/images/buttons/difficulty/easiest/easiest-pressed.png";
+//import easiestPressed from "../assets/images/buttons/difficulty/easiest/easiest-pressed.png";
 import hard from "../assets/images/buttons/difficulty/hard/hard.png";
-import hardPressed from "../assets/images/buttons/difficulty/hard/hard-pressed.png";
+//import hardPressed from "../assets/images/buttons/difficulty/hard/hard-pressed.png";
 export default {
   name: "Main menu",
   setup: function () {
+    const buttons = ref({
+        easiest,
+        easy,
+        notSoEasy,
+        hard
+    })
+    
     const handleChoice = (input) => {
-       console.log(input,'handleChoice called')
+      console.log(input, "handleChoice called");
     };
     const addShadow = (input) => {
-        console.log(input,'addShadow called')
-    }
+      console.log(input, "addShadow called");
+    };
     const changeImage = (input) => {
-        console.log(input,'changeImage called')
-    }
-
-    const mainMenuStyle = {
-      backgroundImage: `url(${mainMenu})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "contain",
-      height: 200,
-      width: 1200,
-      display: "block",
-      position: "relative",
+      console.log(input, "changeImage called");
     };
 
     return {
       handleChoice,
       addShadow,
       changeImage,
-      easy,
-      easyPressed,
-      notSoEasy,
-      notSoEasyPressed,
-      easiest,
-      easiestPressed,
-      hard,
-      hardPressed,
       mainMenu,
-      mainMenuStyle,
+      buttons
     };
   },
 };
 </script>
 <template>
-    <div>
-        <img class="main-menu" :src="mainMenu" alt="">
-        <!-- here the buttons go -->
-        <img class="btn easiest" :src="easiest" alt="" @click="handleChoice(1)" @mousedown="addShadow(1)">
-        <img class="btn easy" :src="easy" alt="" @click="handleChoice(2)" @mousedown="addShadow(2)">
-        <img class="btn not-so-easy" :src="notSoEasy" alt="" @click="handleChoice(3)" @mousedown="addShadow(3)">
-        <img class="btn hard" :src="hard" alt="" @click="handleChoice(4)" @mousedown="addShadow(4)">
-    </div>
+  <div>
+    <img class="main-menu" :src="mainMenu" alt="" />
+    <!-- here the buttons go -->
+    <img
+      class="btn easiest"
+      :src="buttons.easiest"
+      alt=""
+      @click="handleChoice(1)"
+      @mousedown="addShadow('easiest')"
+    />
+    <img
+      class="btn easy"
+      :src="buttons.easy"
+      alt=""
+      @click="handleChoice(2)"
+      @mousedown="addShadow('easy')"
+    />
+    <img
+      class="btn not-so-easy"
+      :src="buttons.notSoEasy"
+      alt=""
+      @click="handleChoice(3)"
+      @mousedown="addShadow('not-so-easy')"
+    />
+    <img
+      class="btn hard"
+      :src="buttons.hard"
+      alt=""
+      @click="handleChoice(4)"
+      @mousedown="addShadow('hard')"
+    />
+  </div>
 </template>
 <style scoped>
 .btn {
-    position: absolute;
-    width: 30vh;
-    height: 9vh;
-    z-index: 1;
-    left: 18vh;
+  position: absolute;
+  width: 30vh;
+  height: 9vh;
+  z-index: 1;
+  left: 18vh;
 }
 .easiest {
-    top: 20vh;
+  top: 20vh;
 }
 .easy {
-    top: 35vh;
+  top: 35vh;
 }
 .not-so-easy {
-    top: 50vh;
+  top: 50vh;
 }
 .hard {
-    top: 65vh;
+  top: 65vh;
 }
-.main-menu{
-    position: absolute;
-    width:  65vh;
-    height: 90vh;
+.main-menu {
+  position: absolute;
+  width: 65vh;
+  height: 90vh;
 }
 </style>
