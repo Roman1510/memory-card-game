c8c1998b6df6e9e27b8a30585b1c343a2c40dcb8
Roman1510 <romanvinnick@gmail.com>
2021-11-24 17:24:07 +0100

    testing slots

---
+++ src/components/TestModal.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <Modal v-if="showModal" @close="showModal = false">
+     <template v-slot:title> Our Title </template>
+      <template> Our Content </template>
+      <template> Our Comments </template>
+    </Modal>
+  </div>
+</template>
+<script>
+import Modal from "./Modal.vue";
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-// import MainMenu from "./components/MainMenu.vue"
+// import TestModal from "./components/TestModal.vue"
 import App from './App.vue'
 createApp(App).mount('#app')
